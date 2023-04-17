59ea4ef397439b17f2de5af848c4a0f43e4a64c5
WeronikaGraczyk <wercia01@onet.pl>
2023-04-17 07:59:20 +0200

    add half working datable on page

--- force-app/main/default/lwc/duplicateComponent/duplicateComponent.js
+++ force-app/main/default/lwc/duplicateComponent/duplicateComponent.js
@@ -5,37 +5,46 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class DuplicateComponent extends LightningElement {
     @api recordId;
     duplicateObjectsList;
-    
+    error;
+
+
+    objects;
+    fields;
+
     @wire(findDuplicates, {idFromPage: '$recordId'})
-    duplicateObjects(results){
-        this.duplicateObjectsList = results;
-        console.log('asdada'+this.duplicateObjectsList);
-    }
-
-    @wire(findDuplicates, { objectName: '$objectName' })
-    duplicates;
-
-    get columns() {
-        console.log('columns '+this.duplicateObjectsList.data.length);
-        if (this.duplicateObjectsList.data && this.duplicateObjectsList.data.length > 0) {
-            // Use the first record in the list to get the field names
-            console.log('columns1 '+this.duplicateObjectsList.data.length);
-            return Object.keys(this.duplicateObjectsList.data[0]).map(field => {
-                return {
-                    label: field,
-                    fieldName: field
-                };
-            });
+    wiredDuplicates({ error, data }) {
+        if (data) {
+            console.log('sdsdd');
+            this.duplicateObjectsList = data;
+            this.objects = data;
+            this.fields = Object.keys(data[0]);
+            console.log(this.duplicateObjectsList);
+            console.log(this.objects);
+            console.log(this.fields);
+        } else if (error) {
+            console.error(error);
         }
-        return [];
     }
 
-    get rows() {
-        if (this.duplicateObjectsList.data) {
-            return this.duplicateObjectsList.data;
-        }
-        return [];
-    }
+    // get columns() {
+    //     if (this.duplicateObjectsList.data && this.duplicateObjectsList.data.length > 0) {
+    //         // Use the first record in the list to get the field names
+    //         return Object.keys(this.duplicateObjectsList.data[0]).map(field => {
+    //             return {
+    //                 label: field,
+    //                 fieldName: field
+    //             };
+    //         });
+    //     }
+    //     return [];
+    // }
+
+    // get rows() {
+    //     if (this.duplicateObjectsList.data) {
+    //         return this.duplicateObjectsList.data;
+    //     }
+    //     return [];
+    // }
 
     loadRecord() {
         const toast = new ShowToastEvent({
@@ -47,7 +56,7 @@ export default class DuplicateComponent extends LightningElement {
     } 
 
     get duplicatesToShow() {
-        console.log('duplicate to show ',this.duplicateObjectsList);
-        return this.duplicateObjectsList !== undefined && this.duplicateObjectsList != null && this.duplicateObjectsList.data && this.duplicateObjectsList.data.length > 0;
-    }//
+        console.log(this.duplicateObjectsList !== undefined && this.duplicateObjectsList != null && this.duplicateObjectsList.data);
+    return this.duplicateObjectsList !== undefined && this.duplicateObjectsList != null && this.duplicateObjectsList.data;
+    }
 }
