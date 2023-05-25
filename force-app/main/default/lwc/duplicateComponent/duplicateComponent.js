d7dfd2ace71ee7b6074cfafe51f5d53ae49be9e8
WeronikaGraczyk <wercia01@onet.pl>
2023-05-25 07:42:14 +0200

    validation and refactoring

--- force-app/main/default/lwc/duplicateComponent/duplicateComponent.js
+++ force-app/main/default/lwc/duplicateComponent/duplicateComponent.js
@@ -3,112 +3,103 @@ import getInitDuplicateRecordSetList from '@salesforce/apex/DuplicateController.
 import mergeObject from '@salesforce/apex/DuplicateController.mergeObject';
 
 export default class DuplicateComponent extends LightningElement {
-    @api 
-    recordId;
-    error;
-    objects;
-    objectsForModal=[];
-    fields;
-    myArray = ['IsDeleted','CreatedDate', 'CreatedById', 'LastModifiedDate', 'LastModifiedById', 'SystemModstamp', 'LastViewedDate', 'LastReferencedDate', 'CleanStatus', 'PhotoUrl', 'OwnerId'];
-        
+  @api 
+  recordId;
+  error;
+  objects;
+  objectsForModal=[];
+  fields;
+  myArray = ['IsDeleted','CreatedDate', 'CreatedById', 'LastModifiedDate', 'LastModifiedById', 'SystemModstamp', 'LastViewedDate', 'LastReferencedDate', 'CleanStatus', 'PhotoUrl', 'OwnerId'];
 
-    @track showModal = false;
-    @track selectedObject = null;
-    @track selectedFields = null;
-    @track idsList = [];
+  @track showModal = false;
+  @track selectedObject = null;
+  @track selectedFields = null;
+  @track idsList = [];
 
-    @wire(getInitDuplicateRecordSetList, { idFromPage: '$recordId' })
-    wiredDuplicates({ error, data }) {
-      if (data && data.length > 0) {
-        this.objects = data.map(obj => ({ ...obj}));
-        this.objects = this.objects.map(field => {
-          return Object.keys(field)
-             .filter(key => !this.myArray.includes(key))
-             .reduce((obj, key) => {
+  @wire(getInitDuplicateRecordSetList, { idFromPage: '$recordId' })
+  wiredDuplicates({ error, data }) {
+  if (data && data.length > 0) {
+    this.objects = data.map(obj => ({ ...obj}));
+    this.objects = this.objects.map(field => {
+      return Object.keys(field)
+            .filter(key => !this.myArray.includes(key))
+            .reduce((obj, key) => {
               obj[key] = field[key];
               return obj;
-            }, {});
-        });
+      }, {});
+    });
 
-        this.fields = Object.keys(data[0]);
-        this.fields = this.fields.filter(field => !this.myArray.includes(field));
+    this.fields = Object.keys(data[0]);
+    this.fields = this.fields.filter(field => !this.myArray.includes(field));
 
-        this.selectedFields = this.fields.map(field => ({ fieldName: field, isSelected: false, value: "" })); 
+    this.selectedFields = this.fields.map(field => ({ fieldName: field, isSelected: false, value: "" })); 
         
-        this.fields = this.fields.filter(field => field != 'Id');
+    this.fields = this.fields.filter(field => field != 'Id');
       
-      } else if (error) {
-        console.error(error);
-      }
+    } else if (error) {
+      console.error(error);
     }
+  }
 
-    get duplicatesToShow() {
-      return this.objects !== undefined && this.objects != null;
-    }
+  get duplicatesToShow() {
+    return this.objects !== undefined && this.objects != null;
+  }
 
-    openModal() {
-      this.selectedObject = null;
-      this.showModal = true;
-    }
+  openModal() {
+    this.selectedObject = null;
+    this.showModal = true;
+  }
 
-    closeModal() {
-      this.showModal = false;
-    }
+  closeModal() {
+    this.showModal = false;
+  }
 
-    handleCheckboxChange(event) {
+  handleCheckboxChange(event) {
       
-      const fieldFromPage = event.target.dataset.field;
-      const objectId = event.target.dataset.objectId;
-      console.log(JSON.stringify(this.selectedFields, (key, value) => {
-        if (Array.isArray(value)) {
-          return [...value];
-        } else {
-          return value;
-        }
-      }));
-      const foundObjectValue = this.objectsForModal.find(object => object.Id == objectId);
-      const indexToUpdate = this.selectedFields.findIndex(item => item.fieldName == fieldFromPage);
+    const fieldFromPage = event.target.dataset.field;
+    const objectId = event.target.dataset.objectId;
+    const foundObjectValue = this.objectsForModal.find(object => object.Id == objectId);
+    const indexToUpdate = this.selectedFields.findIndex(item => item.fieldName == fieldFromPage);
 
-      if (event.target.checked) {
-        if (indexToUpdate !== -1) {
-          if(!this.selectedFields[indexToUpdate].isSelected){
-            this.selectedFields[indexToUpdate].isSelected = true;
-            this.selectedFields[indexToUpdate].value = foundObjectValue[fieldFromPage];
-          }
-          else{
-            event.target.checked = false;
-          }
+    if (event.target.checked) {
+      if (indexToUpdate !== -1) {
+        if(!this.selectedFields[indexToUpdate].isSelected){
+          this.selectedFields[indexToUpdate].isSelected = true;
+          this.selectedFields[indexToUpdate].value = foundObjectValue[fieldFromPage];
         }
-      } else {
-        if (indexToUpdate !== -1) {
-          this.selectedFields[indexToUpdate].isSelected = false;
-          this.selectedFields[indexToUpdate].value = "";
+        else{
+          event.target.checked = false;
         }
       }
-
-    }
-    
-    handleChooseObjectChange(event){
-      const isChecked = event.target.checked;
-      const objectId = event.target.dataset.objectId;
-    
-      if (isChecked) {
-        this.idsList.push(objectId);
-
-      } else {
-        const index = this.idsList.indexOf(objectId);
-        if (index > -1) {
-          this.idsList.splice(index, 1);
-        }
+    } else {
+      if (indexToUpdate !== -1) {
+        this.selectedFields[indexToUpdate].isSelected = false;
+        this.selectedFields[indexToUpdate].value = "";
       }
-      this.objectsForModal=[];
-      this.objects.forEach((obj) => {
-        if (this.idsList.includes(obj.Id)) {
-          const copy = Object.assign({}, obj);
-          this.objectsForModal.push(copy);
-        }
-      });
     }
+  }
+    
+  handleChooseObjectChange(event){
+    const isChecked = event.target.checked;
+    const objectId = event.target.dataset.objectId;
+    
+    if (isChecked) {
+    this.idsList.push(objectId);
+
+    } else {
+      const index = this.idsList.indexOf(objectId);
+      if (index > -1) {
+        this.idsList.splice(index, 1);
+      }
+    }
+    this.objectsForModal=[];
+    this.objects.forEach((obj) => {
+      if (this.idsList.includes(obj.Id)) {
+      const copy = Object.assign({}, obj);
+      this.objectsForModal.push(copy);
+      }
+    });
+  }
     
   handleMergeClick() {
   this.selectedFields = this.selectedFields.filter(field => field.fieldName !== 'Id');
